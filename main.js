5a80f51622a28df39bba174d85edcc9edb89b1a2
ISHANCODERJR <104986815+ISHANCODERJR@users.noreply.github.com>
2023-12-21 20:48:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+// song variable
+
+song1 = ""
+song2 = ""
+song3 = ""
+song4 = ""
+song5 = ""
+song6 = ""
+song7 = ""
+song8 = ""
+song9 = ""
+song10 = ""
+song11 = ""
+song12 = ""
+song13 = ""
+song14 = ""
+song15 = ""
+song16 = ""
+song17 = ""
+song18 = ""
+song19 = ""
+song20 = ""
+song21 = ""
+song22 = ""
+song23 = ""
+song24 = ""
+song25 = ""
+song26 = ""
+song27 = ""
+song28 = ""
+song29 = ""
+song30 = ""
+song31 = ""
+song32 = ""
+song33 = ""
+song34 = ""
+
+// song variables
+
+function setup() {
+    canvas = createCanvas(600 , 530)
+    canvas.center()
+
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+function draw() {
+    image(video , 0 , 0 , 500 , 600)
+}
+
+function priload() {
+    song1 = loadSound("1.mp3")
+    song2 = loadSound("2.mp3")
+    song3 = loadSound("3.mp3")
+    song4 = loadSound("4.mp3")
+    song5 = loadSound("5.mp3")
+    song6 = loadSound("6.mp3")
+    song7 = loadSound("7.mp3")
+    song8 = loadSound("8.mp3")
+    song9 = loadSound("9.mp3")
+    song10 = loadSound("10.mp3")
+    song11 = loadSound("11.mp3")
+    song12 = loadSound("12.mp3")
+    song13 = loadSound("13.mp3")
+    song14 = loadSound("14.mp3")
+    song15 = loadSound("15.mp3")
+    song16 = loadSound("16.mp3")
+    song17 = loadSound("17.mp3")
+    song18 = loadSound("18.mp3")
+    song19 = loadSound("19.mp3")
+    song20 = loadSound("20.mp3")
+    song21 = loadSound("21.mp3")
+    song22 = loadSound("22.mp3")
+    song23 = loadSound("23.mp3")
+    song24 = loadSound("24.mp3")
+    song25 = loadSound("25.mp3")
+    song26 = loadSound("26.mp3")
+    song27 = loadSound("27.mp3")
+    song28 = loadSound("28.mp3")
+    song29 = loadSound("29.mp3")
+    song30 = loadSound("30.mp3")
+    song31 = loadSound("31.mp3")
+    song32 = loadSound("32.mp3")
+    song33 = loadSound("33.mp3")
+    song34 = loadSound("34.mp3")
+}
